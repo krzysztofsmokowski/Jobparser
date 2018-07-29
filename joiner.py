5a2c6fec668a2687460a040d7f7a3118ed319646
krzysztofsmokowski <krzysztofsmokowski@gmail.com>
2018-07-29 14:40:35 +0200

    program completed, using twilio instead of emails

--- joiner.py
+++ joiner.py
@@ -1,47 +1,39 @@
 import requests
 import feedparser
 import configuration
-import smtplib
+from twilio.rest import Client
 
 
 class FeedHandler(object):
     def __init__(self, feed_url, programming_language, location):
         self.url = feed_url
         self.entries_dict = {}
-        self.mail_pwd = configuration.pwd
-        self.mail = configuration.mail
-        self.mail_receiver = configuration.receiver_mail
         self.programming_language = programming_language
         self.location = location
-
+        self.account_sid = configuration.account_sid
+        self.auth_token = configuration.auth_token
 
     def entries_dict_creating(self):
         feed = feedparser.parse(self.url)
         job_list = 'Jobs in {}: '.format(self.location)
         for entries in feed.entries:
             if self.programming_language in entries.title and self.location in entries.summary:
-                #self.entries_dict[entries.title] = entries.summary[len(entries.summary)-40:len(entries.summary)-5]
-                job_list += entries.title
+                job_list += entries.title + '  '
         return job_list
 
-
-    def email_sending(self):
-        '''
-        Method for sending email with information about jobs
-        '''
-        content_to_send = self.entries_dict_creating()
-        mail = smtplib.SMTP('smtp.gmail.com:587')
-        mail.ehlo()
-        mail.starttls()
-        mail.login(self.mail, self.mail_pwd)
-        mail.sendmail(self.mail, self.mail_receiver, content_to_send)
-        mail.close()
+    def twilio_sms_sender(self):
+        client = Client(self.account_sid, self.auth_token)
+        client.messages.create(
+                to=configuration.to,
+                from_=configuration._from,
+                body=self.entries_dict_creating())
+        
 
 
 def main():
     FH = FeedHandler('https://justjoin.it/feed', 'Python', 'Poznań')
-    print(FH.entries_dict_creating())
-    print(FH.email_sending())
+    print(len(FH.entries_dict_creating()))
+    FH.twilio_sms_sender()
 
 if __name__ == '__main__':
     main()
