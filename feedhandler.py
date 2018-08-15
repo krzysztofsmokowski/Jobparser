9e68c358ed9684e7f72c155e5c7607301d2002d6
krzysztofsmokowski <krzysztofsmokowski@gmail.com>
2018-08-15 12:43:31 +0200

    dockerized entire application, using telegram api instead of twilio

--- feedhandler.py
+++ feedhandler.py
@@ -2,6 +2,8 @@ import requests
 import feedparser
 import configuration
 import telepot
+import time
+import logging
 from twilio.rest import Client
 
 
@@ -10,6 +12,8 @@ class FeedHandler(object):
         self.url = feed_url
         self.programming_language = programming_language
         self.location = location
+        self.bot = telepot.Bot(configuration.telegram_auth)
+        logging.getLogger().setLevel(logging.INFO)
 
     def entries_string_creating(self):
         feed = feedparser.parse(self.url)
@@ -27,15 +31,26 @@ class FeedHandler(object):
                 body=self.entries_string_creating())
         return message.sid
         
-
     def telegram_sender(self):
-        bot = telepot.Bot(configuration.telegram_auth)
-        bot.sendMessage(configuration.telegram_id, str(self.entries_string_creating()))
+        self.bot.sendMessage(configuration.telegram_id, str(self.entries_string_creating()))
+        logging.info("message succesfuly sent")
 
+    def jobs_string_compare(self):
+        while True:
+            str_of_jobs = str(self.entries_string_creating())
+            time.sleep(3600)
+            str_to_compare = str(self.entries_string_creating())
+            if str_of_jobs == str_to_compare:
+                pass
+            else:
+                self.telegram_sender()
+                logging.info("new jobs appeared, message sent")
 
+                
 def main():
     FH = FeedHandler('https://justjoin.it/feed', 'Python', 'Poznań')
     print(FH.telegram_sender())
+    FH.jobs_string_compare()
 
 if __name__ == '__main__':
     main()
