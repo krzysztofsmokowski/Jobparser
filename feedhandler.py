63b9133ecd311822671b10c63b6d9de461a25fea
root <root@vps672188.ovh.net>
2020-01-01 13:56:28 +0100

    improved comparement of data sets

--- feedhandler.py
+++ feedhandler.py
@@ -15,35 +15,35 @@ class FeedHandler(object):
         self.bot = telepot.Bot(configuration.telegram_auth)
         logging.getLogger().setLevel(logging.INFO)
 
-    def entries_string_creating(self):
+    def entries_list_creating(self):
         feed = feedparser.parse(self.url)
-        job_list = 'Jobs in {}: '.format(self.location)
+        job_string = 'Jobs in {}: '.format(self.location)
+        job_list = []
         for entries in feed.entries:
             if self.programming_language in entries.title and self.location in entries.summary:
-                job_list += entries.title + ' '
+                job_list.append(entries.title)
+        job_list.sort()
         return job_list
         
     def telegram_sender(self):
-        self.bot.sendMessage(configuration.telegram_id, str(self.entries_string_creating()))
+        self.bot.sendMessage(configuration.telegram_id, 'Jobs in {} '.format(self.location) + str(self.entries_list_creating()))
         logging.info("message succesfuly sent")
 
-    def jobs_string_compare(self):
+    def jobs_list_compare(self):
         while True:
-            str_of_jobs = str(self.entries_string_creating())
-            time.sleep(3600)
-            str_to_compare = str(self.entries_string_creating())
-            if str_of_jobs == str_to_compare:
+            list_of_jobs = self.entries_list_creating()
+            time.sleep(2400)
+            list_to_compare = self.entries_list_creating()
+            if bool(set(list_of_jobs).intersection(list_to_compare)) == True:
                 pass
             else:
                 self.telegram_sender()
                 logging.info("new jobs appeared, message sent")
                 logging.info(datetime.now().isoformat(timespec='hours'))
-
                 
 def main():
     FH = FeedHandler('https://justjoin.it/feed', 'Python', 'Poznań')
-    print(FH.telegram_sender())
-    FH.jobs_string_compare()
+    FH.jobs_list_compare()
 
 if __name__ == '__main__':
     main()
